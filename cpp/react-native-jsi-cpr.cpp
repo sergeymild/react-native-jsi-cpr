21d0417706b557bcc83432d0014b26185b38f6db
Sergei Golishnikov <sergeymild@yandex.ru>
2022-04-02 10:47:26 +0300

    chore: initial commit

---
+++ cpp/react-native-jsi-cpr.cpp
@@ -0,0 +1,7 @@
+#include "react-native-jsi-cpr.h"
+
+namespace example {
+	int multiply(float a, float b) {
+		return a * b;
+	}
+}
---
+++ ios/JsiCpr.h
@@ -0,0 +1,11 @@
+#import <React/RCTBridgeModule.h>
+
+#ifdef __cplusplus
+
+#import "react-native-jsi-cpr.h"
+
+#endif
+
+@interface JsiCpr : NSObject <RCTBridgeModule>
+
+@end
