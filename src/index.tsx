f89d710b0ee5b729fc288833510c7a62f4e1a8c6
SergeyMild <sergeymild@yandex.ru>
2022-06-08 16:14:41 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -16,8 +16,10 @@ import { CurlHelper } from './CurlHelper';
 
 import { NativeModules, Platform } from 'react-native';
 
+export {JsiError, JsiRequest}
+
 const LINKING_ERROR =
-  `The package 'react-native-jsi-websockets' doesn't seem to be linked. Make sure: \n\n` +
+  `The package 'react-native-jsi-cpr' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo managed workflow\n';
