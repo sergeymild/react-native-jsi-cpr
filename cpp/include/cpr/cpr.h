682c6fa778d1547f92d0b30916696d0f46186924
SergeyMild <sergeymild@yandex.ru>
2022-08-13 11:14:57 +0300

    update cpr

--- cpp/include/cpr/cpr.h
+++ cpp/include/cpr/cpr.h
@@ -9,7 +9,6 @@
 #include "cpr/connect_timeout.h"
 #include "cpr/cookies.h"
 #include "cpr/cprtypes.h"
-#include "cpr/cprver.h"
 #include "cpr/curl_container.h"
 #include "cpr/curlholder.h"
 #include "cpr/error.h"
